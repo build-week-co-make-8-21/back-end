98bcc21d27300fa6f9dc306b13bcda88245581de
haileyhansard <haileyhansard@gmail.com>
2020-08-26 10:20:43 -0700

    getting ready for testing

--- api/server.test.js
+++ api/server.test.js
@@ -3,13 +3,4 @@ const request = require('supertest');
 const server = require('./server.js');
 const db = require('../data/db-config.js');
 
-describe('server', () => {
-    describe('issues', () => {
-        it('should add a new user', async () => {
-            await request(server).post('/login').setEncoding({
-                "username": "hailey",
-                "password": "pass123"
-            })
-        })
-    })
-});
+
