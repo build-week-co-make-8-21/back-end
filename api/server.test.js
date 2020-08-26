649db20929e60883902422fc216f474c32606110
Dominique K Kitchenakow <domikitchen@users.noreply.github.com>
2020-08-26 16:55:41 -0500

    added the issue tests and part of the comments tests

--- api/server.test.js
+++ api/server.test.js
@@ -5,11 +5,140 @@ const db = require('../data/db-config.js');
 
 describe('server', () => {
     describe('issues', () => {
-        it('should add a new issue', async () => {
-            await request(server).post('/login').send({
-                "username": "hailey",
-                "password": "pass123"
+        describe('get', () => {
+            it('get /api/issues', async () => {
+                const res = await request(server).post('/login').send({
+                    "username": "hailey",
+                    "password": "pass123"
+                })
+                const usertoken = res.body.token;
+    
+    
+                const issues = await request(server).get('/api/issues').set({ Authorization: usertoken });
+    
+                expect(issues.status).toBe(200);
+                expect(issues.body).toHaveLength(3);
             });
-        })
-    })
+            it('get /api/issues/id', async () => {
+                const res = await request(server).post('/login').send({
+                    "username": "hailey",
+                    "password": "pass123"
+                })
+                const usertoken = res.body.token;
+    
+                const issue = await request(server).get('/api/issues/1').set({ Authorization: usertoken });
+    
+                expect(issue.status).toBe(200);
+                expect(issue.type).toBe('application/json');
+            });
+        });
+        describe('post', () => {
+            it('post /api/issues', async () => {
+                const res = await request(server).post('/login').send({
+                    "username": "hailey",
+                    "password": "pass123"
+                })
+                const usertoken = res.body.token;
+    
+                const issue = await request(server).post('/api/issues').send({
+                    "title": "new title",
+                    "description": "new description",
+                    "categoryId": 2
+                }).set({ Authorization: usertoken });
+    
+                expect(issue.status).toBe(201);
+            });
+        });
+        describe('put', () => {
+            it('put /api/issues/id', async () => {
+                const res = await request(server).post('/login').send({
+                    "username": "hailey",
+                    "password": "pass123"
+                })
+                const usertoken = res.body.token;
+    
+                const issue = await request(server).put('/api/issues/3').send({
+                    "title": "edited title",
+                    "description": "edited description",
+                    "categoryId": 3
+                }).set({ Authorization: usertoken });
+    
+                expect(issue.status).toBe(203);
+            });
+        });
+        describe('delete', () => {
+            it('delete /api/issues/id', async () => {
+                const res = await request(server).post('/login').send({
+                    "username": "hailey",
+                    "password": "pass123"
+                })
+                const usertoken = res.body.token;
+
+                const issues = await request(server).get('/api/issues').set({ Authorization: usertoken });
+
+                await issues.body.map(async (issue) => {
+                    if(issue.issueId > 3) {
+                       await request(server).delete(`/api/issues/${issue.issueId}`).set({ Authorization: usertoken });
+                    }
+                    else {
+                        expect(issues.body).toHaveLength(4)
+                    }
+                })
+
+                expect(issues.body).toHaveLength(4);
+            });
+        });
+    });
+    describe('comments', () => {
+        describe('get', () => {
+            it('get /api/issues/comments', async () => {
+                const res = await request(server).post('/login').send({
+                    "username": "hailey",
+                    "password": "pass123"
+                })
+                const usertoken = res.body.token;
+    
+                const comments = await request(server).get('/api/issues/comments').set({ Authorization: usertoken });
+    
+                expect(comments.status).toBe(200);
+            });
+            it('get /api/issues/id/comments', async () => {
+                const res = await request(server).post('/login').send({
+                    "username": "hailey",
+                    "password": "pass123"
+                })
+                const usertoken = res.body.token;
+    
+                const comments = await request(server).get('/api/issues/1/comments').set({ Authorization: usertoken });
+    
+                expect(comments.status).toBe(200);
+            });
+            it('get /api/issues/comments/id', async () => {
+                const res = await request(server).post('/login').send({
+                    "username": "hailey",
+                    "password": "pass123"
+                })
+                const usertoken = res.body.token;
+    
+                const comments = await request(server).get('/api/issues/comments/1').set({ Authorization: usertoken });
+    
+                expect(comments.status).toBe(200);
+            })
+        });
+        describe('post', () => {
+            it('post /api/issues/id/comments', async () => {
+                const res = await request(server).post('/login').send({
+                    "username": "hailey",
+                    "password": "pass123"
+                })
+                const usertoken = res.body.token;
+
+                const comment = await request(server).post('/api/issues/1/comments').send({
+                    "comment": "new comment"
+                }).set({ Authorization: usertoken });
+
+                expect(comment.status).toBe(201);
+            });
+        });
+    });
 });
