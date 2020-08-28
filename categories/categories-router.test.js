b35c82b10e2968ba12d69c10033f7393b239ee0a
haileyhansard <haileyhansard@gmail.com>
2020-08-28 00:50:40 -0700

    Added put and delete tests passing on categories-router

--- categories/categories-router.test.js
+++ categories/categories-router.test.js
@@ -18,7 +18,7 @@ describe('categories-router', () => {
             return usertoken = res.body.token;
         });
 
-        it('should get categories', async () => {
+        it('should login, use token, return status 200 OK and return json data', async () => {
             const categories = await request(server)
                 .get('/api/categories')
                 .set({ Authorization: usertoken })
@@ -26,18 +26,18 @@ describe('categories-router', () => {
             expect(categories.status).toBe(200);
             expect(categories.type).toMatch(/json/i)
         });
-
-        it('should get category by id', async () => {
+        
+        it('should get category by id, and return json data', async () => {
             const oneCategory = await request(server)
-                .get('/api/categories/1')
-                .set({ Authorization: usertoken })
-
+            .get('/api/categories/1')
+            .set({ Authorization: usertoken })
+            
             expect(oneCategory.type).toMatch(/json/i);
         });            
     });
 
     describe('POST a new category', () => {
-        it('should post a new category', async () => {
+        it('should login, use token, post a new category', async () => {
             const res = await request(server)
                 .post('/login')
                 .send({
@@ -48,14 +48,65 @@ describe('categories-router', () => {
 
             const newCategory = await request(server)
                 .post('/api/categories')
+                .set({ Authorization: usertoken })
                 .send({
                     categoryId: 1,
                     categoryName: "Test Category Name"
                 })
-                .set({ Authorization: usertoken });
+                .expect(201)
+                expect(newCategory.type).toMatch(/json/i);
+        });
+    });
 
-            expect(newCategory.type).toMatch(/json/i);
-            expect(newCategory.status).toBe(201)
+    describe('PUT update a categoryName', () => {
+        it('should login, use token to update,  return status 200 OK', async () => {
+            const res = await request(server)
+                .post('/login')
+                .send({
+                    username: "test",
+                    password: "pass123"
+                })
+            const usertoken = res.body.token;
+
+            const updateCategory = await request(server) 
+                .put('/api/categories/1')
+                .set({ Authorization: usertoken })
+                .send({
+                    categoryName: "Updated Category"
+                })
+
+            expect(updateCategory.status).toBe(200)
+        });        
+        it('should login, use token to update, return success message', async () => {
+            const res = await request(server)
+                .post('/login')
+                .send({
+                    username: "test",
+                    password: "pass123"
+                })
+            const usertoken = res.body.token;
+
+            const updateCategory2 = await request(server) 
+                .put('/api/categories/1')
+                .set({ Authorization: usertoken })
+                .send({
+                    categoryName: "Updated Category"
+                })
+
+            expect(updateCategory2.body.message).toContain("Category updated successfully")
+        });
+    });
+
+    describe('Cannot DELETE a category WITHOUT token', () => {
+        it('should return status 200 OK when category is deleted', async () => {
+    
+            const deleteCategory = await request(server)
+                .delete('/api/categories/4')
+                .send()
+
+            expect(deleteCategory.status).toBe(401)
+            expect(deleteCategory.type).toMatch(/json/i);
+            expect(deleteCategory.error.message).toContain("cannot DELETE /api/categories/4")
         });
     });
 });
