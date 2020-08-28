6e947b884cd6006638ae4e5d5c4968b6aa2c91cf
haileyhansard <haileyhansard@gmail.com>
2020-08-27 23:39:28 -0700

    categories-router test passing

--- categories/categories-router.test.js
+++ categories/categories-router.test.js
@@ -1,29 +1,61 @@
 const request = require('supertest');
-
-const server = require('../api/server');
-const db = require('../data/db-config.js');
-const { intersect } = require('../data/db-config.js');
-const { default: expectCt } = require('helmet/dist/middlewares/expect-ct');
-
+const server = require("../api/server");
+const db = require('../data/db-config');
 
 describe('categories-router', () => {
-    let token
     beforeEach(async () => {
-        const res = await request(server)
-        .post('/login')
-        .send({ "username": 'hailey', "password": 'pass123' })
-        .then(res => {
-            token = res.body.token
-        })
-        await db('users')
-    })
+        await db('categories').truncate();
+    });
+
     describe('GET /api/categories', () => {
-        it('should respond with status 200 OK', () => {
-            return request(server)
-            .get('/api/categories')
-            .then(res => {
-                expect(res.status).toBe(201);
-            })
-        })
-    })
-})
+        beforeEach(async () => {
+            const res = await request(server)
+                .post('/login')
+                .send({
+                    "username": "test",
+                    "password": "pass123"
+                })
+            return usertoken = res.body.token;
+        });
+
+        it('should get categories', async () => {
+            const categories = await request(server)
+                .get('/api/categories')
+                .set({ Authorization: usertoken })
+
+            expect(categories.status).toBe(200);
+            expect(categories.type).toMatch(/json/i)
+        });
+
+        it('should get category by id', async () => {
+            const oneCategory = await request(server)
+                .get('/api/categories/1')
+                .set({ Authorization: usertoken })
+
+            expect(oneCategory.type).toMatch(/json/i);
+        });            
+    });
+
+    describe('POST a new category', () => {
+        it('should post a new category', async () => {
+            const res = await request(server)
+                .post('/login')
+                .send({
+                    username: "test",
+                    password: "pass123"
+                })
+            const usertoken = res.body.token;
+
+            const newCategory = await request(server)
+                .post('/api/categories')
+                .send({
+                    categoryId: 1,
+                    categoryName: "Test Category Name"
+                })
+                .set({ Authorization: usertoken });
+
+            expect(newCategory.type).toMatch(/json/i);
+            expect(newCategory.status).toBe(201)
+        });
+    });
+});
